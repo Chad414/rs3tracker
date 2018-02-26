e2b03cd1383b2b9b984fa587497b6242ced865ea
Chad Hamdan <chad.hamdan4@gmail.com>
2018-02-26 11:58:21 -0500

    Init
    New Xcode Project

---
+++ RS3-Toolkit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RS3-Toolkit
+//
+//  Created by Chad Hamdan on 2/26/18.
+//  Copyright © 2018 Chad Hamdan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
