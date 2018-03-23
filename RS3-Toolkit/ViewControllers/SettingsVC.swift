727ac65a72e1ff1abfbfc91519419b4c90ab9c6d
Chad414 <chad.hamdan4@gmail.com>
2018-03-23 17:52:25 -0400

    RC1
    - Completion of SettingsVC & WelcomeVC

--- RS3-Toolkit/ViewControllers/SettingsVC.swift
+++ RS3-Toolkit/ViewControllers/SettingsVC.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class SettingsVC: UIViewController {
-
+    
+    @IBAction func icons8Link(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string: "https://icons8.com")!)
+    }
     
     @IBAction func done(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
--- RS3-Toolkit/ViewControllers/WelcomeVC.swift
+++ RS3-Toolkit/ViewControllers/WelcomeVC.swift
@@ -12,18 +12,60 @@ class WelcomeVC: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var usernameTextInput: UITextField!
     
+    let activityIndicator = UIActivityIndicatorView()
+    var updating: Bool = false {
+        didSet {
+            if updating {
+                startLoading()
+            } else {
+                stopLoading()
+            }
+        }
+    }
+    
+    var store = UserDataStore()
+    
+    var userDataFound: Bool = false {
+        didSet {
+            if userDataFound {
+                Global.cachedUserData = nil
+                Global.cachedUserAvatar = nil
+                UserDefaults.standard.set(username, forKey: "username")
+                performSegue(withIdentifier: "continue", sender: self)
+            }
+        }
+    }
+    
+    func updateUserData() {
+        store.fetchUserData() {
+            (result) in
+            
+            switch result {
+            case .success:
+                self.userDataFound = true
+                self.updating = false
+            case .failure:
+                print("Failed to Fetch User Data")
+                self.updating = false
+                // Show Alert Here
+                let errorMessage = "The user either doesn't exist, has been offline for a while, or has privacy enabled."
+                let ac = UIAlertController(title: "User Not Found", message: errorMessage, preferredStyle: .alert)
+                let closeAction = UIAlertAction(title: "Close", style: .cancel, handler: nil)
+                ac.addAction(closeAction)
+                self.present(ac, animated: true, completion: nil)
+            }
+        }
+    }
+    
     var username: String {
         let username = usernameTextInput.text ?? ""
         return username.replacingOccurrences(of: " ", with: "_")
     }
     
     @IBAction func continueButton(_ sender: UIButton) {
-        UserDefaults.standard.set(username, forKey: "username")
-        Global.cachedUserData = nil
-        Global.cachedUserAvatar = nil
+        updating = true
         Global.username = username
-        
-        performSegue(withIdentifier: "continue", sender: self)
+        updateUserData()
     }
     
     override func viewDidLoad() {
@@ -35,6 +77,28 @@ class WelcomeVC: UIViewController, UITextFieldDelegate {
     
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
         usernameTextInput.resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        usernameTextInput.resignFirstResponder()
+        return true
+    }
+    
+    func startLoading() {
+        activityIndicator.center = self.view.center;
+        activityIndicator.hidesWhenStopped = true;
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray;
+        let transform: CGAffineTransform = CGAffineTransform(scaleX: 3.5, y: 3.5)
+        activityIndicator.transform = transform
+        view.addSubview(activityIndicator);
+        
+        activityIndicator.startAnimating();
+        UIApplication.shared.beginIgnoringInteractionEvents();
+    }
+    
+    func stopLoading() {
+        activityIndicator.stopAnimating()
+        UIApplication.shared.endIgnoringInteractionEvents()
     }
     
 }
